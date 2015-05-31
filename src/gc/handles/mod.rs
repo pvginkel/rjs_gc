8618a4ce2feba68fa322e57532f993e896b08516
Pieter van Ginkel <pvginkel@gmail.com>
2015-05-31 07:17:57 +0200

    Reorganized handles.

---
+++ src/gc/handles/mod.rs
@@ -0,0 +1,13 @@
+mod array_local;
+mod array_root;
+mod array;
+mod local;
+mod ptr;
+mod root;
+
+pub use self::array_local::ArrayLocal;
+pub use self::array_root::ArrayRoot;
+pub use self::array::Array;
+pub use self::local::Local;
+pub use self::ptr::Ptr;
+pub use self::root::Root;
